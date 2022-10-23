bf3d8c02151ac8ecd5dacaa0542dace62139036b
NicoleArtig <115376634+NicoleArtig@users.noreply.github.com>
2022-10-22 19:56:45 -0600

    Add files via upload

---
+++ Data-Table_Artiga Ramos Gladys Nicole/js/script.js
@@ -0,0 +1,38 @@
+function Receta1() {
+        
+    Swal.fire({
+        imageUrl: 'img/1t.jpg',
+        imageWidth: 1200,
+        imageAlt: ''
+    })
+    
+}
+function Receta2() {
+        
+  Swal.fire({
+      imageUrl: 'img/2l.jpg',
+      imageWidth: 1200,
+      imageAlt: ''
+  })
+  
+}
+function Receta3() {
+        
+  Swal.fire({
+      imageUrl: 'img/3c.jpg',
+      imageWidth: 1200,
+      imageAlt: ''
+  })
+  
+}
+function Favoritos() {
+
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Añanido a favoritos',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    
+}
